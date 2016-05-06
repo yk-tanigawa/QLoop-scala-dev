4c24d4c1e62060d760e5db6c883c94212b4eacd1
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2016-05-07 00:22:01 +0900

    Add some code to compute k-mer Freq vector

--- src/main/scala/QLoop.scala
+++ src/main/scala/QLoop.scala
@@ -22,10 +22,25 @@ object QLoop {
 
     val fasta = new Fasta(testFasta)
 
-
     val f = new Feature(fasta.toStr, 5)
-
     println(f.f)
+
+
+    val res = 1
+    val k = 1
+    val mar = 1
+
+    val f_kmer_freq = {
+      (res: Int, k: Int, mar:Int, seq:String, i : Int) =>
+        new Feature(seq slice (i * res - mar, (i + 1) * res + k - 1 + mar), k)
+    }.curried
+
+
+
+    val vs = (1 until fasta.len - 1).map(f_kmer_freq(res)(k)(mar)(fasta.toStr))
+
+
+    print(vs)
   }
 
 }
