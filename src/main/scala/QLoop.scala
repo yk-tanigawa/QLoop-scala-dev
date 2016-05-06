c5b4b28048a961b3ae111673872ee8418f590112
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2016-05-06 15:29:02 +0900

    Initial Commit

---
+++ src/main/scala/QLoop.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by yosuke on 5/6/16.
+  */
+object QLoop {
+  val progName = "QLoop"
+  val version = "1.0"
+
+  def showInfo(): Unit ={
+    println(progName)
+  }
+
+  def main(args: Array[String]) {
+    showInfo()
+
+  }
+
+}
